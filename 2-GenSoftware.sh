46335d911f08efabd19aa352b9e3385346682429
James Tutton <github@comexe.co.uk>
2021-08-03 16:52:51 +0100

    Update 2-GenSoftware.sh

--- 2-GenSoftware.sh
+++ 2-GenSoftware.sh
@@ -163,7 +163,7 @@ execute sudo apt-get install firefox -y
 #Brave
 execute sudo apt install apt-transport-https curl -y
 execute sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
-execute echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list
+echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list
 execute sudo apt update
 execute sudo apt install brave-browser -y
 
